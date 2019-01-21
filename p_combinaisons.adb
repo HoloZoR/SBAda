ddef60df3857a8eb1824ce8119e5c94e241425f7
LEDOYEN Baptiste <ledoyenb@hostname.iut2.upmf-grenoble.fr>
2019-01-21 15:57:52 +0100

    Debug V1 15h57

--- p_combinaisons.adb
+++ p_combinaisons.adb
@@ -43,15 +43,15 @@ end TriVectGaudi;
 procedure CreeFicsol(V : in TV_Gaudi; fsol : in out text_io.file_type) is
 -- {f ouvert en �criture, V Tri� par nom de case}
 --	=> 	{fsol contient toutes les combinaisons gagnantes et est structuré selon le format défini (cf. sujet)}
-  nb_case : integer;
-  i1,i2,i3,i4,i5,i6,i7 : integer;
-  nb_combi3,nb_combi4,nb_combi5,nb_combi6,nb_combi7 : integer;
+  fsol3, fsol4, fsol5, fsol6, fsol7 : text_io.file_type;
+  nb_combi3,nb_combi4,nb_combi5,nb_combi6,nb_combi7 : integer := 0;
   somme : integer;
-  val : string 1..14;
+  val : string(1..14);
   lg : integer;
 begin
   reset(fsol, out_file);
   create(fsol3, out_file, "fsol3.txt");
+  create(fsol4, out_file, "fsol4.txt");
   create(fsol5, out_file, "fsol5.txt");
   create(fsol6, out_file, "fsol6.txt");
   create(fsol7, out_file, "fsol7.txt");
@@ -59,7 +59,7 @@ begin
     for i2 in i1+1..V'Last loop
 -- 3 cases
       for i3 in i2+1..V'Last loop
-        somme := V(i1).valeur+V(i2).valeur+V(i3).valeur
+        somme := V(i1).valeur+V(i2).valeur+V(i3).valeur;
         if somme = 33 then
           put(fsol3,i1);
           put(fsol3,i2);
@@ -68,7 +68,7 @@ begin
         elsif somme < 33 then
 -- 4 cases
           for i4 in i3+1..V'Last loop
-            somme := V(i1).valeur+V(i2).valeur+V(i3).valeur+V(i4).valeur
+            somme := V(i1).valeur+V(i2).valeur+V(i3).valeur+V(i4).valeur;
             if somme = 33 then
               put(fsol4,V(i1).nom);
               put(fsol4,V(i2).nom);
@@ -78,7 +78,7 @@ begin
             elsif somme < 33 then
 -- 5 cases
               for i5 in i4+1..V'Last loop
-                somme := V(i1).valeur+V(i2).valeur+V(i3).valeur+V(i4).valeur+V(i5).valeur
+                somme := V(i1).valeur+V(i2).valeur+V(i3).valeur+V(i4).valeur+V(i5).valeur;
                 if somme = 33 then
                   put(fsol5,V(i1).nom);
                   put(fsol5,V(i2).nom);
@@ -89,7 +89,7 @@ begin
                 elsif somme < 33 then
 -- 6 cases
                   for i6 in i5+1..V'Last loop
-                    somme := V(i1).valeur+V(i2).valeur+V(i3).valeur+V(i4).valeur+V(i5).valeur+V(i6).valeur
+                    somme := V(i1).valeur+V(i2).valeur+V(i3).valeur+V(i4).valeur+V(i5).valeur+V(i6).valeur;
                     if somme = 33 then
                       put(fsol6,V(i1).nom);
                       put(fsol6,V(i2).nom);
@@ -101,7 +101,7 @@ begin
                     elsif somme < 33 then
 -- 7 cases
                       for i7 in i6+1..V'Last loop
-                        somme := V(i1).valeur+V(i2).valeur+V(i3).valeur+V(i4).valeur+V(i5).valeur+V(i6).valeur+V(i7).valeur
+                        somme := V(i1).valeur+V(i2).valeur+V(i3).valeur+V(i4).valeur+V(i5).valeur+V(i6).valeur+V(i7).valeur;
                         if somme = 33 then
                           put(fsol7,V(i1).nom);
                           put(fsol7,V(i2).nom);
@@ -189,19 +189,17 @@ begin
   nb := 3;
   --------------------------
   if nbcases = 3 then
-    get_line(f, nbcombi,nb);
+    get_line(fsol, nbcombi,nb);
   else -- nbcases /= 3
     for i in 3..nbcases loop
       skip_page(fsol); -- saute les pages jusqu'à la page voulu
     end loop;
   end if;
   --------------------------
-  get_line(f, nbcombi,nb);
+  get_line(fsol, nbcombi,nb);
   --------------------------
   return integer'value(nbCombi(3..nb));
   --------------------------
-  exception
-    when END_ERROR => return integer'value(nbCombi(3..nb)); -- nbcases > 7
 end nbCombi;
 ------------------------------Combi---------------------------------------------
 function Combi(fsol : in text_io.file_type; nbcases : in T_nbcases; numsol : in positive) return string is
@@ -210,7 +208,7 @@ function Combi(fsol : in text_io.file_type; nbcases : in T_nbcases; numsol : in 
   nbcombi : string(1..14);
   nb : natural;
 begin
-  nbCombi := "0 0";
+  nbCombi := "0 0           ";
   nb := 3;
   --------------------------
   if nbcases /= 3 then
@@ -222,17 +220,17 @@ begin
   skip_line(fsol); -- saute la ligne avec solutions et cases
   --------------------------
   for j in 1..numsol loop
-    get_line(f, nbcombi,nb);
+    get_line(fsol, nbcombi,nb);
   end loop;
   --------------------------
   return nbCombi(nbCombi'First..nb);
   ---------fin--------------
 end Combi;
 ------------------------------nbCombi-------------------------------------------
-function est_contigue(sol : in string) return boolean is
-  --{sol repr�sente une solution} => {r�sultat = vrai si sol est une solution contig�e}
-begin
-
-end est_contigue;
+-- function est_contigue(sol : in string) return boolean is
+--   --{sol repr�sente une solution} => {r�sultat = vrai si sol est une solution contig�e}
+-- begin
+--
+-- end est_contigue;
 ----------------------------fin-------------------------------------------------
 end p_combinaisons;
