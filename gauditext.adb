1f5c483af3cc2a716eeb68bdcb0811584cd4d740
LEDOYEN Baptiste <ledoyenb@hostname.iut2.upmf-grenoble.fr>
2019-01-22 12:08:44 +0100

    commencement affichage et mise a jour global

--- gauditext.adb
+++ gauditext.adb
@@ -1,7 +1,9 @@
-
+with p_esiut, p_vue_text; use p_esiut, p_vue_text;
 procedure gauditext is
-
-
+  nbcases : T_nbcases;
 begin -- Procedure
-
+  while true loop
+    ecrire_ligne("Choisissez un nombre de cases : "); lire(T_nbcases);
+    afficheGrille();
+  end loop;
 end gauditext;
--- p_vue_text.ads
+++ p_vue_text.ads
@@ -1,5 +1,14 @@
 package p_vue_text is
-
+  --------------------------------procedure-------------------------------------
+  --------------------------------afficheEtoile---------------------------------
+  procedure afficheEtoile(nb in integer);
+  -- {} => {Affiche *-----* de nb occurence}
+  --------------------------------afficheBarre---------------------------------
+  procedure afficheBarre(nb in integer);
+  -- {} => {Affiche |     | de nb occurence}
+  --------------------------------afficheGrille---------------------------------
+  procedure afficheGrille(V : in TV_Gaudi);
+  --{V trié sur nom} => {Affiche la grille d'origine}
 
 
 
