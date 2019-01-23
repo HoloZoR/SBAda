9a5175269a698dc4e0676ad2c99bf20b7e384484
fadlnndd <fanny.delnondedieu@gmail.com>
2019-01-23 10:46:32 +0100

    test

--- p_combinaisons.adb
+++ p_combinaisons.adb
@@ -264,77 +264,99 @@
 
     end Affichage;
 
-
-    function contigue2cases(C1, C2 : in string) return boolean is
-    --{sol repr�sente une solution} => {r�sultat = vrai si 2 cases sont contigues}
-        col1, col2 : character;
-        lig1, lig2 : T_Lig;
-    begin
-        col1 := C1(C1'First);
-        lig1 := T_Lig'Value('0' & C1(C1'First + 1));
-        col2 := C2(C2'First);
-        lig2 := T_Lig'Value('0' & C2(C2'First + 1));
-          return((col1 = col2 and (lig1 = lig2 +1 or lig1 = lig2 -1))
-          or (lig1 = lig2 and (col1 = T_Col'succ(col2) or col1 = T_Col'pred(col2)))
-          or ((lig1 = lig2 +1 or lig1 = lig2 -1) and (col1 = T_Col'succ(col2) or col1 = T_Col'pred(col2))));
-
-    end contigue2cases;
-
-    function indFalse(V : in TV_etatCase) return integer is
-    --{} => { retourne la valeur du première indice à false}
-      i : integer := V'First;
-    begin
-      while i < V'last + 1 and then V(i) /= 0 loop
-        i := i + 1;
-      end loop;
-      return i;
-    end indFalse;
-    function checkCont(V : in TV_etatCase) return boolean is
-    --{} => { retourne si les éléments sont contigue}
-    begin
-      return indFalse(v) = V'last;
-    end indFalse;
-
-
-    function est_contigue(sol : in string) return boolean is
-      --{sol repr�sente une solution} => {r�sultat = vrai si sol est une solution contig�e}
-      ----- declatation de mes variables
-      Vc : TV_Case(1..(sol'length/2)); -- vecteur de la taille de sol/2 possédent les cases
-      Ve : TV_etatCase(1..(sol'length/2)); -- vecteur de la taille de sol/2 possédent l'état d'une cases
-      i,j : positive := V'first;
-      indVal : positive := V'first;
-      contigue : boolean := false;
-    begin
-      -- Initialisation de V --
-      while i < Vc'last + 1 loop
-        Vc(i) := sol(j)&sol(j+1);
-        i := i + 1;
-        j := j + 2;
-      end loop; -- Vecteur initiliser par les cases de sol et contigue = true
-      Ve := (true, others => false);
-      ------------------------------------------------------------------------
-      while not contigue loop
-        j := indFalse(Ve)
-        if contigue2cases(indVal, j) then
-          Ve(j) := true;
-          indVal := indVal + 1;
-        else
-        end if;
-        contigue = checkCont(Ve);
-      end loop;
-
-    end est_contigue;
-
+    -- 
+    -- function contigue2cases(C1, C2 : in string) return boolean is
+    -- --{sol repr�sente une solution} => {r�sultat = vrai si 2 cases sont contigues}
+    --     col1, col2 : character;
+    --     lig1, lig2 : T_Lig;
+    -- begin
+    --     col1 := C1(C1'First);
+    --     lig1 := T_Lig'Value('0' & C1(C1'First + 1));
+    --     col2 := C2(C2'First);
+    --     lig2 := T_Lig'Value('0' & C2(C2'First + 1));
+    --       return((col1 = col2 and (lig1 = lig2 +1 or lig1 = lig2 -1))
+    --       or (lig1 = lig2 and (col1 = T_Col'succ(col2) or col1 = T_Col'pred(col2)))
+    --       or ((lig1 = lig2 +1 or lig1 = lig2 -1) and (col1 = T_Col'succ(col2) or col1 = T_Col'pred(col2))));
+    --
+    -- end contigue2cases;
+    --
+    -- function indFalse(V : in TV_etatCase) return integer is
+    -- --{} => { retourne la valeur du première indice à false}
+    --   i : integer := V'First;
+    -- begin
+    --   while i < V'last + 1 and then V(i) /= 0 loop
+    --     i := i + 1;
+    --   end loop;
+    --   return i;
+    -- end indFalse;
+    -- function checkCont(V : in TV_etatCase) return boolean is
+    -- --{} => { retourne si les éléments sont contigue}
+    -- begin
+    --   return indFalse(v) = V'last;
+    -- end indFalse;
+    --
+    --
+    -- function est_contigue(sol : in string) return boolean is
+    --   --{sol repr�sente une solution} => {r�sultat = vrai si sol est une solution contig�e}
+    --   ----- declatation de mes variables
+    --   Vc : TV_Case(1..(sol'length/2)); -- vecteur de la taille de sol/2 possédent les cases
+    --   Ve : TV_etatCase(1..(sol'length/2)); -- vecteur de la taille de sol/2 possédent l'état d'une cases
+    --   i,j : positive := V'first;
+    --   indVal : positive := V'first;
+    --   contigue : boolean := false;
+    -- begin
+    --   -- Initialisation de V --
+    --   while i < Vc'last + 1 loop
+    --     Vc(i) := sol(j)&sol(j+1);
+    --     i := i + 1;
+    --     j := j + 2;
+    --   end loop; -- Vecteur initiliser par les cases de sol et contigue = true
+    --   Ve := (true, others => false);
+    --   ------------------------------------------------------------------------
+    --   while not contigue loop
+    --     j := indFalse(Ve)
+    --     if contigue2cases(indVal, j) then
+    --       Ve(j) := true;
+    --       indVal := indVal + 1;
+    --     else
+    --     end if;
+    --     contigue = checkCont(Ve);
+    --   end loop;
+    --
+    -- end est_contigue;
+    --
     -- procedure CreeFicsolcont(fsol, fcont : in out text_io.file_type)  is
     -- -- {fsol ouvert} => {fcont contient les combinaisons contig�es de fsol et est structur� de la m�me fa�on}
     --   val : string(1..15);
     --   lg : integer;
+    --   type TV_Contigu is array (positive range <>) of string;
+    --   V : TV_Contigu (1..35);
+    --   i : integer;
+    --   nbcases : integer;
     -- begin
     --   reset(fsol, in_file);
     --   reset(fcont, out_file);
+    --   nbcases := 3;
     --   while not end_of_file(fsol) loop
-    --     skip_line()
-    --     get_line(fsol,val,lg)
+    --     i := V'First;
+    --     skip_line(fsol);
+    --     while not end_of_page(fsol) loop
+    --       get_line(fsol,val,lg);
+    --       if est_contigue(val(1..lg)) then
+    --         V(i) := val(1..lg);
+    --         i := i+1;
+    --       end if;
+    --     end loop;
+    --     put(fcont, nbcases, 1);
+    --     put(fcont,' ');
+    --     put(fcont, i, 1);
+    --     for j in 1..i loop
+    --       put_line(fcont, V(j));
+    --
+    --     end loop;
+    --     new_page(fcont);
+    --     nbcases := nbcases +1;
+    --
     --   end loop;
     -- end CreeFicsolcont;
 
--- p_combinaisons.ads
+++ p_combinaisons.ads
@@ -14,8 +14,8 @@ package p_combinaisons is
 	end record;
 	----------------déclarations de vecteur pour partie 3-------------------------
 	----- vecteur de Case -----
-	type TV_Case is array (positive range <>) of string;
-	type TV_etatCase is array (positive range <>) of boolean;
+	-- type TV_Case is array (positive range <>) of string;
+	-- type TV_etatCase is array (positive range <>) of boolean;
 
 	---- Instanciation de sequential_IO pour le fichier de description de la grille ---------------------------------
 	package p_cases_io is new sequential_IO (TR_Case); use p_Cases_IO;
@@ -48,18 +48,18 @@ package p_combinaisons is
 
 	procedure Affichage(fsol : in out text_io.file_type; nbcases : in T_nbcases);
 	-- {fsol ouvert} => {Affiche toutes les combinaisons pour un nombre de case donnés}
-
-	function contigue2cases(C1, C2 : in string) return boolean;
-	--{sol repr�sente une solution} => {r�sultat = vrai si 2 cases sont contigues}
-
-	function checkCont(V : in TV_etatCase) return boolean is
-	--{} => { retourne si les éléments sont contigue}
-
-	function indFalse(V : in TV_etatCase) return integer;
-	--{} => { retourne la valeur du première indice à false}
-
-	function est_contigue(sol : in string) return boolean;
-		--{sol repr�sente une solution} => {r�sultat = vrai si sol est une solution contig�e}
+	--
+	-- function contigue2cases(C1, C2 : in string) return boolean;
+	-- --{sol repr�sente une solution} => {r�sultat = vrai si 2 cases sont contigues}
+	--
+	-- function checkCont(V : in TV_etatCase) return boolean is
+	-- --{} => { retourne si les éléments sont contigue}
+	--
+	-- function indFalse(V : in TV_etatCase) return integer;
+	-- --{} => { retourne la valeur du première indice à false}
+	--
+	-- function est_contigue(sol : in string) return boolean;
+	-- 	--{sol repr�sente une solution} => {r�sultat = vrai si sol est une solution contig�e}
 
 	-- procedure CreeFicsolcont(fsol, fcont : in out text_io.file_type) ;
 	-- -- {fsol ouvert} => {fcont contient les combinaisons contig�es de fsol et est structur� de la m�me fa�on}
