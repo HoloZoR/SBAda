33e6753ef5fbab3032b1f68fd1d21bb01a0d2fef
LEDOYEN Baptiste <ledoyenb@hostname.iut2.upmf-grenoble.fr>
2019-01-24 16:07:15 +0100

    Accueil et passage a deuxième fenêtre clean

--- gaudigraph.adb
+++ gaudigraph.adb
@@ -2,32 +2,29 @@ with p_combinaisons; use p_combinaisons;
 with p_vue_graph; use p_vue_graph;
 with p_fenbase; use p_fenbase;
 with forms; use forms;
-
+with p_esiut; use p_esiut;
 
 procedure gaudigraph is
   Facc,Fcrypt : TR_Fenetre;
   s : string(1..20);
-  num : integer;
+  nbcases : integer;
+  Btcontigue, BtCommencer : boolean;
 begin -- gaudigraph
   -----------------Initialisation des fenêtres----------------------------------
   InitialiserFenetres;
   -- Fenêtre d'accueil
   FenetreAccueil(Facc);
   --Fenêtre cryptogramme
-  montrerfenetre(facc);
-  s := attendreBouton(facc);
+  FenetreCryptogramme(Fcrypt);
   ------------------------------------------------------------------------------
-  Fcrypt := DebutFenetre("Cryptogramme de Subirachs",800,700);
-    AjouterTexte(Fcrypt, "Titre","Cryptogramme de Subirachs", 200,20, 400, 30);
-    AjouterTexte(Fcrypt, "FondCrypto", "", 175, 70, 450, 450);
-    ChangerCouleurFond(Fcrypt, "FondCrypto", FL_BLACK);
-    AfficheGrille(Fcrypt);
-    AjouterBouton(Fcrypt, "pred", "Precedant", 50,550, 200,100);
-    AjouterBouton(Fcrypt, "succ", "Suivant", 550,550, 200,100);
-    AjouterTexte(Fcrypt, "numsol", "1", 350,500, 150, 150);
-
-  FinFenetre(Fcrypt);
+  montrerfenetre(facc);
+  nbcases := 3; Btcontigue := false; BtCommencer := False;
+  while not BtCommencer loop
+    choixdesCase(Facc,attendreBouton(facc),nbcases, Btcontigue, BtCommencer);
+    ecrire_ligne(nbcases);
+    ecrire_ligne(Btcontigue);
+    ecrire_ligne(BtCommencer);
+  end loop;
   montrerfenetre(Fcrypt);
-  AfficheSolution(Fcrypt, "A1A2B3C4");
-  Solution(Fcrypt, num, 33, attendreBouton(Fcrypt));
+  s := attendreBouton(FCrypt);
 end gaudigraph;
--- p_vue_graph.ads
+++ p_vue_graph.ads
@@ -10,9 +10,16 @@ package p_vue_graph is
   procedure FenetreAccueil(Facc : in out TR_Fenetre);
   -- {} => {Creation de la fenetre d'accueil}
 
+  procedure FenetreCryptogramme(Fcrypt : in out TR_Fenetre);
+  -- {} => {Creation de la fenetre du Cryptogramme}
+
+  procedure choixdesCase(Facc : in out TR_Fenetre; attClick : in string; nbcases : in out integer; Btc : in out boolean; BtCommencer : in out boolean);
+  --{} => {Enregistre le dernier choix et case}
+
   procedure Solution(Fen : in out TR_Fenetre; num : out integer; nbcb : in integer;s : in string);
+  --{} => {}
 
   procedure AfficheSolution (Fen : in out TR_Fenetre; sol : in string);
-
+  --{} => {}
 
 end p_vue_graph;
